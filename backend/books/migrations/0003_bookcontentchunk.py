e5fed9047bce6c409592cec2b354cec852ec3899
mibana1 <ksy661004@gmail.com>
2026-06-16 17:11:25 +0900

    [Fix] 0002_bookcontentchunk.py->0003_bookcontentchunk.py
    기존에 002가 있어서 충돌나는 버그 수정

--- backend/books/migrations/0003_bookcontentchunk.py
+++ backend/books/migrations/0003_bookcontentchunk.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("books", "0001_initial"),
+        ("books", "0002_alter_book_isbn"),
     ]
 
     operations = [
